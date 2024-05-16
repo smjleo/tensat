3999d9c77bae04d716aa1b14f061e0d06772950e
Arya Vohra <arya.vohra04@gmail.com>
2024-05-16 10:02:18 -0500

    C++ interface now working

--- build.rs
+++ build.rs
@@ -9,6 +9,9 @@ fn main() {
     println!("cargo:rerun-if-changed=include/tensat.h");
 
     // C++ graph input bindings
-    // cxx_build::bridge("src/input.rs")
-    //     .compile("tensatcpp");
+    cxx_build::bridge("src/input.rs")
+        .flag_if_supported("-std=c++20")
+        .flag_if_supported("-lc++")   // Link libc++ explicitly
+        .flag_if_supported("-lc++abi") // Link libc++abi if needed
+        .compile("tensatcpp");
 }
--- run_graph_cc.sh
+++ run_graph_cc.sh
@@ -1,1 +1,1 @@
-g++ -std=c++20 -o tensatcpp src/bert.cc -I target/cxxbridge -Ltarget/debug -ltensat -ltaso_runtime && ./tensatcpp
+g++ -std=c++20 -o tensatcpp src/bert.cc -I target/cxxbridge -I target/debug/build/tensat/out -L target/debug -ltensat -lc++ -lc++abi && ./tensatcpp
