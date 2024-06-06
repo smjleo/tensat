5fee1869afca6f3dddf3b26804cb15b72a84dab2
Arya Vohra <arya.vohra04@gmail.com>
2024-06-06 00:35:44 -0400

    shapeless

--- src/graph.cc
+++ src/graph.cc
@@ -5,10 +5,15 @@
 
 int main() {
     auto graphBox = new_converter();
-    int dims[2] = {1024, 1024};
-    auto input_slice = rust::Slice<const int32_t>{dims, 2};
-    auto inp = graphBox->new_input(input_slice);
-    auto relu = graphBox->new_tanh_op(*inp, 0);
+    // int dims[2] = {1024, 1024};
+    int dims_1[0] = {};
+    int dims_2[0] = {};
+    auto input_slice_1 = rust::Slice<const int32_t>{dims_1, 0};
+    auto input_slice_2 = rust::Slice<const int32_t>{dims_2, 0};
+    auto inp1 = graphBox->new_input(input_slice_1);
+    auto inp2 = graphBox->new_input(input_slice_2);
+    auto mul = graphBox->new_mul_op(*inp1, *inp2, 0);
+    // auto relu = graphBox->new_tanh_op(*inp, 0);
 
     graphBox->print_rec_expr();
 }
